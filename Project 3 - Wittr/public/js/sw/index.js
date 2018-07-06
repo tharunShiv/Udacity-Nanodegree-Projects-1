43ebcbfc6af760e3b3d0ddad48b8432bc1e6accd
tharunShiv <tharun.rocky14@gmail.com>
2018-07-06 16:58:54 +0530

    urls cached and fetched

--- Project 3 - Wittr/public/js/sw/index.js
+++ Project 3 - Wittr/public/js/sw/index.js
@@ -1,7 +1,31 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
+    // Ensure at least $primary-color changes
+    // TODO: change cache name to 'wittr-static-v2'
+    caches.open('wittr-static-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    // TODO: remove the old cache
+  );
+});
+
 self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
   event.respondWith(
-    fetch('/imgs/dr-evil.gif')
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
